7eacf7ff181586509de9c6006206f3918d9f2a4d
chiakikogure <chiaki.kogretch@gmail.com>
2025-09-08 23:45:53 +0900

    Update script.js

--- schoolFes_202510/js/script.js
+++ schoolFes_202510/js/script.js
@@ -281,6 +281,7 @@ topBar.querySelector(".menuOpen_button").addEventListener("click", () => {
         const href = getHref(item[2]);
         content.href = href;
         const fileName = window.location.href.split("/").pop();
+        console.log(fileName, href)
         if (fileName === "" ? "index.html" : fileName === href) {
             content.style.pointerEvents = "none";
             content.style.opacity = .5;
